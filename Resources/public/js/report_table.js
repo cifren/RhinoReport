7698972dbed4efbe7716f483f8d8d339298dab6e
flecoq <cifren@msn.com>
2014-05-05 15:39:21 -0700

    FUL-28 - remove headerFixed on report, library doesnt work anymore Add new elements in readMe

--- Resources/public/js/report_table.js
+++ Resources/public/js/report_table.js
@@ -1,11 +1,3 @@
 $(document).ready(function() {
     $("#report_table").collapseTable();
-        
-    var xthf = []; 
-    xAddEventListener(window, 'load',
-        function() {
-            //table class 'header-fixed', class 'top-bar' is header at the top of the page, menu etc...
-            xthf[0] = new xTableHeaderFixed('header-fixed', window, xHeight('top-bar'));
-        }, false
-    ); 
 });
