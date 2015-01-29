7d2813d5e240861d53a27a7d66d5d223783023d3
cifren <cifren@msn.com>
2015-01-29 16:31:30 -0600

    ChartInit - add templating folder in order to be able to switch if needed

---
+++ Module/Table/Templating/DataTableTemplate/Generator/HtmlTableTemplateGenerator.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Earls\RhinoReportBundle\Module\Table\Templating\DefaultTemplate\Generator;
+
+use Earls\RhinoReportBundle\Report\Templating\DefaultTemplate\Generator\HtmlTemplateGenerator;
+use Earls\RhinoReportBundle\Module\Table\Templating\DefaultTemplate\Simplifier\HtmlReportSimplifier;
+use Earls\RhinoReportBundle\Module\Table\Templating\DefaultTemplate\HtmlTemplateTable;
+use Earls\RhinoReportBundle\Report\ReportObject\ModuleObject;
+
+class HtmlTableTemplateGenerator extends HtmlTemplateGenerator
+{
+
+    protected $template;
+
+    public function getResponse($nameFile, ModuleObject $table, $arg)
+    {
+        return $this->renderView($this->template, array_merge(array('table' => $simpleTable, 'js' => true, 'css' => true), $arg));
+    }
+
+    public function getTemplating(ModuleObject $reportObject, $remoteUrl, $exportUrl)
+    {
+        $template = new HtmlTemplateTable();
+        $transformedObject = $this->applyTransformers($reportObject);
+        
+        $simplifier = new HtmlReportSimplifier($reportObject);
+        $simpleTable = $simplifier->getSimpleTable();
+        $template->setSimpleTable($simpleTable);
+        
+        $template->setModuleObject($transformedObject);
+        $template->setRemoteUrl($remoteUrl);
+        $template->setExportUrl($exportUrl);
+        
+        return $template;
+    }
+
+}
